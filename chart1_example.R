6decd4c592be3036e7082caa8601a52e4070fc55
1uf3n <lufanw@uw.edu>
2023-08-03 00:20:26 -0700

    1

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,27 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(tidyverse)
+library(dplyr)
+library(readr)
+
+prison <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true")
+
+top_states <- prison %>%
+  group_by(state) %>%
+  summarise(total_pop = sum(total_pop, na.rm = TRUE)) %>%
+  arrange(desc(total_pop)) %>%
+  slice(1:5) %>%
+  pull(state)
+
+data <- prison %>%
+  filter(state %in% top_states)
+
+ggplot(data) +
+  geom_line(mapping = aes(
+    x = year,
+    y = total_pop,
+    color = state
+  )) +
+  labs(title = "Trend in Total Population of Prisoners for Top 5 States",
+       x = "Year",
+       y = "Total Population",
+       color = "Top States")
